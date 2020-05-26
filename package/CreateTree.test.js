2046f3ed125e8a7b7e52712605084f5f602dd9ab
RudoH <rudohengst@gmail.com>
2020-05-26 10:59:18 -0400

    minor fixes re testing

--- package/CreateTree.test.js
+++ package/CreateTree.test.js
@@ -2,64 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
 // import chronoscope from 'react-chronoscope';
-import createTree from './createTree';
+import createTree from './CreateTree';
 
-  function Dialog({children}){
-    return(
-      <div>
-        <p>Hello</p>
-        {children}
-      </div>
-    )
-  }
-
-  function Button({children}){
-    return (
-      <button>
+function Dialog({ children }) {
+  return (
+    <div>
+      <p>Hello</p>
       {children}
-      </button>
-    )
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return <button>{children}</button>;
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Dialog>
+          <Button>Click me</Button>
+        </Dialog>
+      </div>
+    );
   }
+}
 
-  class App extends React.Component{
-    render() {
-      return (
-        <div>
-          <Dialog>
-            <Button>
-              Click me
-            </Button>
-          </Dialog>
-        </div>
-      )
+let container;
 
-    }
-  }
-
-  let container;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
-  it('test treeGraph', () => {
-
-    act(() =>{
-      console.log("container -", container);
-      ReactDOM.render(<App />, container);
-    });
-
-    const treeGraph = createTree(container);
-    console.log("treeGraph -", JSON.stringify(treeGraph));
-
-    expect(container.innerHTML).toBe('<div><div><p>Hello</p><button>Click me</button></div></div>')
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
+it('test treeGraph', () => {
+  act(() => {
+    console.log('container -', container);
+    ReactDOM.render(<App />, container);
+  });
 
+  const treeGraph = createTree(container);
+  console.log('treeGraph -', JSON.stringify(treeGraph));
+
+  expect(container.innerHTML).toBe(
+    '<div><div><p>Hello</p><button>Click me</button></div></div>'
+  );
+});
--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello Playground</h1>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello Playground</h1>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'));
