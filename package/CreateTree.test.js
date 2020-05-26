382de99f9d846ef7a3d1243280917f2d71cb040b
lpham598 <lpham598@gmail.com>
2020-05-26 18:57:58 -0400

    formatting and removed comments

--- package/CreateTree.test.js
+++ package/CreateTree.test.js
@@ -1,93 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
-// import chronoscope from 'react-chronoscope';
 import createTree from './createTree';
 import TicTacToe from '../mockData/ticTacToe'
 
-  function Dialog({children}){
-    return(
-      <div>
-        <p>Hello</p>
-        {children}
-      </div>
-    )
-  }
-  function Button({children}){
-    return (
-      <button>
-      {children}
-      </button>
-    )
-  }
-  class App extends React.Component{
-    render() {
-      return (
-        <div>
-          <Dialog>
-            <Button>
-              Click me
-            </Button>
-          </Dialog>
-        </div>
-      )
+let container;
+let treeGraph;
+  
+describe('test treeGraph', () => {
+  
+  beforeAll(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Dialog>
-          <Button>Click me</Button>
-        </Dialog>
-      </div>
-    );
-  }
-  
-  let container;
-  let treeGraph;
-  
-  describe('test treeGraph', () => {
-    
-    beforeAll(() => {
-      container = document.createElement('div');
-      document.body.appendChild(container);
-      act(() => {
+    act(() => {
       ReactDOM.render(<TicTacToe />, container);
-      console.log("container -", container);
-      // console.log("treeGraph -", JSON.stringify(treeGraph));
-      });
+    });
+
     treeGraph = createTree(container);
-    })
-
-
-    // beforeEach(() => {
-      //   container = document.createElement('div');
-      //   document.body.appendChild(container);
-      // });
-      
-    afterAll(() => {
-      document.body.removeChild(container);
-      container = null;
-    });     
-    it("Test if 3 row elements are created on the page", () => {
-      // console.log('inside first it treegraph: ', treeGraph)
-      expect(treeGraph.children[0].children[1].children.length).toBe(3); // ==> 3 (number of Rows)
-    });
-    it("Test if the third row also has 3 elements (buttons)", () => {
-      expect(treeGraph.children[0].children[1].children[0].children.length).toBe(3); // ==> 3 (number of Bixes in the row)
-    });
   })
-    // expect(container.innerHTML).toBe('<div><div><p>Hello</p><button>Click me</button></div></div>')
 
+ 
+  afterAll(() => {
+    document.body.removeChild(container);
+    container = null;
+  });     
 
-      // console.log('inside first it treegraph: ', treeGraph)
-      expect(treeGraph.children[0].children[1].children.length).toBe(3); // ==> 3 (number of Rows)
-    });
-    it("Test if the third row also has 3 elements (buttons)", () => {
-      expect(treeGraph.children[0].children[1].children[0].children.length).toBe(3); // ==> 3 (number of Bixes in the row)
-    });
-  })
-    // expect(container.innerHTML).toBe('<div><div><p>Hello</p><button>Click me</button></div></div>')
+  it("Test if 3 row elements are created on the page", () => {
+
+    expect(treeGraph.children[0].children[1].children.length).toBe(3); 
+  });
+
+  it("Test if the third row also has 3 elements (buttons)", () => {
+    expect(treeGraph.children[0].children[1].children[0].children.length).toBe(3); 
+  });
+})
+
+    expect(treeGraph.children[0].children[1].children.length).toBe(3); 
+  });
+
+  it("Test if the third row also has 3 elements (buttons)", () => {
+    expect(treeGraph.children[0].children[1].children[0].children.length).toBe(3); 
+  });
+})
+
 
 
 
@@ -98,9 +54,4 @@ it('test treeGraph', () => {
   });
 
   const treeGraph = createTree(container);
-  console.log('treeGraph -', JSON.stringify(treeGraph));
-
-  expect(container.innerHTML).toBe(
-    '<div><div><p>Hello</p><button>Click me</button></div></div>'
-  );
 });
