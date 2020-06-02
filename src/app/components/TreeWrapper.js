d6e6aa5ab3f4917291d9a05dab9ab84cdbee01c9
RudoH <rudohengst@gmail.com>
2020-06-02 11:11:30 -0400

    reverted treewrapper in reald3 branch

--- src/app/components/TreeWrapper.js
+++ src/app/components/TreeWrapper.js
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
 import D3Tree from './D3Tree';
 
-let root;
-
 export default class TreeWrapper extends Component {
-  constructor(props) {
-    super(props);
-    this.treeRef = React.createRef();
-  }
-
   componentDidMount() {
-    const { name, children } = this.props;
-    const hierarchy = { name, children };
-    root = JSON.parse(JSON.stringify(hierarchy));
-    this.maked3Tree();
-  }
-
-  componentDidUpdate() {
-    const { name, children } = this.props;
-    const hierarchy = { name, children };
-    root = JSON.parse(JSON.stringify(hierarchy));
-    this.maked3Tree();
+    this.setState({
+      tree: new D3Tree(this.refs.tree),
+    });
   }
 
   render() {
-    return (
-      <div>
-        <div ref={this.treeRef}></div>
-      </div>
-    );
+    return <div ref="tree"></div>;
   }
 }
