65e13f569966c06dd128a1bac37a1fd23b531d6b
lpham598 <lpham598@gmail.com>
2020-06-04 10:18:52 -0400

    in progress flamegraph

--- src/app/components/flameChart.js
+++ src/app/components/flameChart.js
@@ -1,13 +1,37 @@
 import React, {Component} from 'react';
+import * as flamegraph from 'd3-flame-graph';
 
 class FlameChart extends Component {
   constructor(props) {
   super(props);
+  this.flamegraphRef = React.createRef();
+  this.createFlameGraph = this.createFlameGraph.bind(this);
+  }
+
+  componentDidMount(){
+    this.createFlameGraph()
+  }
+
+  componentDidUpdate(){
+    this.createFlameGraph(stats)
+  }
+
+  createFlameGraph() {
+    flamegraph.destroy();
+    const chart = flamegraph()
+      .width(960);
+
+    d3.json("data.json", function(error, data) {
+      if (error) return console.warn(error);
+      d3.select("#flamegraph")
+        .datum(data)
+        .call(chart);
+    })
   }
 
   render() { 
     return (
-    <div>
+    <div ref={this.flamegraphRef} className="flamegraph">
         In progress: Flame Chart
     </div>  
     );
