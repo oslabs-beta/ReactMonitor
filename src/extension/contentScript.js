2fd02fbad924c1ec472582077c4eca3c95e701f9
Dan <bitsmith_ny19@danigil.com>
2020-07-23 07:01:25 -0500

    last mod

--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -9,3 +9,20 @@ window.addEventListener('message', (msg) => {
     });
   }
 });
+/*
+console.log("*".repeat(40), "BEFORE MESSAFGE");
+window.setTimeout( function() {
+	// test - inject javascript, access fibergraph
+	const body0 = document.body;
+	const s0 = document.createElement('script');
+	s0.setAttribute('type', 'text/javascript');
+	s0.innerText = `
+		console.log("¡¡ejecutado!!");
+		let tree = document.querySelector("#container");
+		let test0 = tree._reactRootContainer._internalRoot
+			.current.alternate.tag;
+		console.log(test0);
+	`;
+	body0.appendChild(s0);
+}, 1000 );
+*/
