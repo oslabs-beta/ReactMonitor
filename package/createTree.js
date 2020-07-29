11ba3814736224db9647f60a99cd23cbf5e8525b
Dan <bitsmith_ny19@danigil.com>
2020-07-29 14:04:50 -0500

    eliminated changes from  this commit related to the problem of the definition of hostRoot; purpose - to separate pull requests to different issues

--- package/createTree.js
+++ package/createTree.js
@@ -3,7 +3,7 @@ const makeTreeCreator = require('./makeTreeCreator');
 
 module.exports = function (container) {
   const fiberRoot = container._reactRootContainer._internalRoot;
-  let hostRoot = fiberRoot.current;
+  const hostRoot = fiberRoot.current;
   const treeCreator = makeTreeCreator();
 
   // on first load use initial render.
@@ -13,15 +13,11 @@ module.exports = function (container) {
     setTimeout(() => {
       sendContentScript(treeCreator, hostRoot, fiberRoot.current);
     }, 200);
-	// to be overwritten by next pull request:
-	hostRoot = fiberRoot.current;
   });
 
   window.addEventListener('keyup', () => {
     setTimeout(() => {
       sendContentScript(treeCreator, hostRoot, fiberRoot.current);
-		// to be overwritten by next pull request:
-		hostRoot = fiberRoot.current;
     }, 200);
   });
 };
--- package/treeGraphFactory.js
+++ package/treeGraphFactory.js
@@ -5,28 +5,32 @@
  module.exports = function (fiber){
 
     const helper = (fiber, treeGraph) => {
-	// 2 different types of react build I saw - 
-	//		1 - react element is also leaf in fibertree
-	//		2 - has 1 additional child, with type == null
-	function null_node(node)
-	{
-		return ( (node == null) || (
-			(node.child == null) &&
-				(node.type == null)
-		) );
-	}
+  // 2 different types of leaves observed in different
+  // react builds:
+  //   1 - a leaf react elements is also a leaf in fibertree
+  //   2 - given a leaf react element, the fiber tree has
+  //     has 1 additional child bellow it
+  //     and it's type is null.
+  // given that obervation, null_node() detects both cases:
+  function null_node(node)
+  {
+    return ( (node == null) || (
+      (node.child == null) &&
+        (node.type == null)
+    ) );
+  }
       // check if fiber.child !== null - traverse
-	/* if child: create node and enter into child */
+  /* if child: create node and enter into child */
       if (fiber.child) {
         // push the new Node to the treeGraph.children array
         // the parent will the tree graph we are currently working with
         // (do the type check for elements that are functions or html elements)
         let newGraphNode = treeGraph;
-		// typeof child.type == object <==> child is null 
-		// condition: fiber child && fiber.ch.ch exists && type != null
-		// 			<==> fiber.ch.ch is a leaf (?)
-		if ( !null_node(fiber.child) )
-		{
+    // typeof child.type == object <==> child is null 
+    // condition: fiber child && fiber.ch.ch exists && type != null
+    //       <==> fiber.ch.ch is a leaf (?)
+    if ( !null_node(fiber.child) )
+    {
           newGraphNode = new Node(
             fiber.child.key
             || (fiber.child.type ? fiber.child.type.name : fiber.child.type)
@@ -40,9 +44,9 @@
       }
       // check if fiber.sibling !== null - traverse
       if (fiber.sibling)
-		{
+    {
         let newGraphNode = treeGraph;
-			if ( !null_node(fiber.sibling) ) {
+      if ( !null_node(fiber.sibling) ) {
           // create new GraphNode based on it with parent being a treeGraph.parent
           newGraphNode = new Node(
             fiber.sibling.key
@@ -50,7 +54,7 @@
             || fiber.sibling.type,
             treeGraph.parent, [], fiber.sibling
           );
-			// added - the if condition 
+      // added - the if condition 
           // push the node on to the treeGraph.parent.children array
           /*if (treeGraph.parent)*/ treeGraph.parent.children.push(newGraphNode);
           helper(fiber.sibling, newGraphNode);
