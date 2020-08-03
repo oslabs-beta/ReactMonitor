3f2849732f52e2fcc21b13553cabcbdee2ead26b
naylinnpkv <kathir.pkv@gmail.com>
2020-08-03 15:39:46 -0400

    scripts bundled using webpack, scripts being injected properly to the app

--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -13,23 +13,40 @@ window.addEventListener("message", (msg) => {
 function injectScript(file, node) {
   // test - inject javascript, access fibergraph
   const body0 = document.getElementsByTagName(node)[0];
-  const s0 = document.createElement("script"); //<script type= 'text/javascript' src= ''> consol.log("script injected")</script>
+  for (let i = 0; i < file.length; i++) {
+    const s0 = document.createElement("script"); //<script type= 'text/javascript' src= 'packag'> consol.log("script injected")</script>
 
-  s0.setAttribute("type", "text/javascript");
-  s0.setAttribute("src", chrome.extension.getURL(file));
-  // console.log(chrome.extension.getURL(file[0]), "injectedfile");
-  s0.innerText = `
-		console.log("script injected!!");
-		let tree = document.querySelector("#root");
-		let test0 = tree._reactRootContainer._internalRoot
-			.current.alternate.tag;
-		console.log(test0);
-	`;
-  body0.appendChild(s0);
+    s0.setAttribute("type", "text/javascript");
+    s0.setAttribute("src", chrome.extension.getURL(file[i]));
+    // console.log(chrome.extension.getURL(file[0]), "injectedfile");
+    body0.appendChild(s0);
+  }
 }
+/*
+... this will inject the following HTML into index.html:
+<script type='text/javascript' src="package/newNode.js">`console.log("script injected!!");console.log("yes, script injected");
+    const car = "toyota"`;</script>
+    
+1st type:
+    <script src="..."></script>
+
+2nd type:
+<script>
+.... javascript instructions ...
+</scipt>
+
+s0.innerText = `console.log("script injected!!");
+    console.log("yes, script injected");
+    const car = "toyota";
+`
+
+*/
 
 setTimeout(() => {
-  injectScript("/test.js", "body");
+  injectScript(["/test.js", "/fiberTreeAnalyzer.js"], "body");
 }, 1000);
 
 //what is the script element that will be injected to the HTML
+
+// let tree = document.querySelector("#root");
+// let test0 = tree._reactRootContainer._internalRoot.current.alternate.tag;
---
+++ src/extension/test.js
@@ -0,0 +1,4 @@
+console.log("Hello, I am test.js");
+const car = "TOYOTA";
+const root = document.querySelector("#root");
+console.log(root._reactRootContainer._internalRoot.current.tag);
